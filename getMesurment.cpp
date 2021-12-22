989552e7b9e596213090e967d3ccbbb67beca0f5
nykl <nykl.jan.333@gmail.com>
2021-12-22 12:23:18 +0100

    +

--- getMesurment.cpp
+++ getMesurment.cpp
@@ -82,9 +82,9 @@ PhData readIOGen(Common env, unsigned char addrA, unsigned char addrB) {
 int runIO(Common env) {
     char nullBuff[2];
     if (ioctl(env.vBus, I2C_SLAVE, ADDR_V) < 0 ||
-    write(env.vBus, "\x01\x00\xA0", 2) < 0 ||
-    write(env.vBus, "\x00", 1) < 0 || 
-     read(env.vBus, nullBuff, 2)/* 
+    write(env.vBus, "\x01\x00\xA0", 3) < 0 ||
+    write(env.vBus, "\x00", 1) < 0 /* || 
+     read(env.vBus, nullBuff, 2) *//* 
         write(env.vBus, VOLTAGE_CONFIG, 3) < 0 ||
         write(env.vBus, READ_REG, 1) < 0 *//*  ||
         read(env.vBus, nullBuff, 2)  *//* ||
