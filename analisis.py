6367df563c44c4aef218fe57843a7115b7eb8f56
nykl <nykl.jan.333@gmail.com>
2022-02-27 17:44:29 +0100

    fast up

--- analisis.py
+++ analisis.py
@@ -108,29 +108,30 @@ def getAvrgPower(shiftedBatch):
         cB = []
         v = []
         for i, val in enumerate(phase):
-            powA.append(val[0] * val[2])
-            powB.append(val[1] * val[2])
+            powA.append(val[0] * val[2] * 325)
+            powB.append(val[1] * val[2] * 325)
             cA.append(val[0])
             cB.append(val[1])
-        powA_alt = getAvrgAbs(cA) * 230
-        powB_alt = getAvrgAbs(cB) * 230
+        powA_alt = getAvrgAbs(cA) * 325
+        powB_alt = getAvrgAbs(cB) * 325
         ret.append([powA_alt, powB_alt, getAvrg(powA), getAvrg(powB)])
     return ret
 
 def scaleBatchToReal(batch):
     for phase in batch:
         for i, val in enumerate(phase):
-            phase[i][0] = val[0] / 0x800 * 4.12
-            phase[i][1] = val[1] / 0x800 * 4.12
-            phase[i][2] = val[2] * 0.282069314
+            # old multiplier 4.12
+            phase[i][0] = val[0] / 0x800 * 25.125
+            phase[i][1] = val[1] / 0x800 * 25.125
+            phase[i][2] = val[2] * 0.282069314 # TO PEEK 1
     return batch
 
 def scaleBatch(batch):
     for phase in batch:
         for i, val in enumerate(phase):
-            phase[i][0] = val[0] / 0x800 * 4.12
-            phase[i][1] = val[1] / 0x800 * 4.12
-            phase[i][2] = val[2] * 0.0008671875
+            phase[i][0] = val[0] / 0x800 * 25.125
+            phase[i][1] = val[1] / 0x800 * 25.125
+            phase[i][2] = val[2] * 0.0008671875 # TO PEEK 1
     return batch
             
             
