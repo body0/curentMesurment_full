119e9f4c9982f365e7e5a8aff2c79d7d5c6eb91f
nykl <nykl.jan.333@gmail.com>
2022-08-18 15:31:42 +0200

    auto

--- src/mqtt.py
+++ src/mqtt.py
@@ -33,7 +33,7 @@ def getVoltageTopic(id=0):
     return f'/voltage_now/{id}'
 
 def formatPhaseData(pd, i):
-    f"{pd['curent_chA'][i]},{pd['curent_chB'][i]},{pd['voltage'][i]},{pd['t_curent_chA'][i]},{pd['t_curent_chB'][i]},{pd['t_voltage'][i]}"
+    return f"{pd['curent_chA'][i]},{pd['curent_chB'][i]},{pd['voltage'][i]},{pd['t_curent_chA'][i]},{pd['t_curent_chB'][i]},{pd['t_voltage'][i]}"
 
 def publish(phaseList):
     global client
